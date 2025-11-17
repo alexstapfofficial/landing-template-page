b0651076f0698ee7c88d4ebf80ad33ec1685d5c2
Alexander Stapf <alexanderstapf@Alexanders-MacBook-Pro.fritz.box>
2025-11-17 16:32:25 +0100

    add nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,27 @@ export default defineNuxtConfig({
     '@nuxt/hints',
     '@nuxt/image',
     '@nuxt/ui',
+    'nuxt-studio'
   ],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  },
+
+  studio: {
+    // Studio admin route (default: '/_studio')
+    route: '/_studio',
+    
+    // GitHub repository configuration (owner and repo are required)
+    repository: {
+      provider: 'github', // only GitHub is currently supported
+      owner: 'alexstapfofficial', // your GitHub username or organization
+      repo: 'landing-template', // your repository name
+      branch: 'page', // the branch to commit to (default: main)
+      rootDir: '' // optional: if your Nuxt app is in a subdirectory (default: '')
+    }
+  }
 })
